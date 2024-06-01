19c4c96081e9d744376f4716b268a5e8ac52cd9f
snikuz12 <snikuz12@naver.com>
2024-06-02 00:56:07 +0900

    create table

--- stack_table.sql
+++ stack_table.sql
@@ -1,22 +1,19 @@
-
+-- 스택 카테고리 테이블
 CREATE TABLE IF NOT EXISTS `stack_category` (
-    `category_id` INT(11) NOT NULL AUTO_INCREMENT,
-    `category_name` VARCHAR(100) NOT NULL,
-    PRIMARY KEY (`category_id`)
-) ENGINE = InnoDB AUTO_INCREMENT = 1 DEFAULT CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin;
+    `category_id` INT(11) NOT NULL AUTO_INCREMENT,  -- 카테고리 ID (자동 증가, 기본 키)
+    `category_name` VARCHAR(100) NOT NULL,  -- 카테고리 이름 (NULL 불가)
+    PRIMARY KEY (`category_id`)  -- 기본 키 설정
+);
 
+-- 스택 테이블
 CREATE TABLE IF NOT EXISTS `stack` (
-    `stack_id` BIGINT(20) NOT NULL AUTO_INCREMENT,
-    `stack_category_id` INT(11) NOT NULL,
-    `stack_name` VARCHAR(100) NOT NULL,
-    PRIMARY KEY (`stack_id`),
-    UNIQUE INDEX `stack_name` (`stack_name` ASC) VISIBLE,
-    INDEX `stack_category_id_idx` (`stack_category_id` ASC) VISIBLE,
-    CONSTRAINT `stack_category_id`
-      FOREIGN KEY (`stack_category_id`)
-      REFERENCES `project`.`stack_category` (`category_id`)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION
-) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin;
+    `stack_id` BIGINT(20) NOT NULL AUTO_INCREMENT,  -- 스택 ID (자동 증가, 기본 키)
+    `stack_category_id` INT(11) NOT NULL,  -- 스택 카테고리 ID (NULL 불가)
+    `stack_name` VARCHAR(100) NOT NULL,  -- 스택 이름 (NULL 불가)
+    PRIMARY KEY (`stack_id`),  -- 기본 키 설정
+    UNIQUE INDEX `stack_name` (`stack_name` ASC),  -- 스택 이름의 유니크 인덱스
+    FOREIGN KEY (`stack_category_id`)  -- 외래 키 설정: 스택 카테고리 ID
+    REFERENCES `stack_category` (`category_id`)  -- 참조: 스택 카테고리 테이블의 카테고리 ID
+);
 
-
+ 
