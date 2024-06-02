d0e116f58fae24d07c42e33bab8e435b7c554a84
snikuz12 <snikuz12@naver.com>
2024-06-02 16:59:26 +0900

    pro_pr_post-chat insert data

---
+++ insert-data/pro_pr_post-chat_insert.sql
@@ -0,0 +1,27 @@
+-- 자기소개테이블 데이터 삽입
+INSERT INTO `linkpro`.`pro_pr_post` (`pr_post_id`, `pr_pro_id`, `pr_title`, `pr_contents`, `pr_github`) VALUES ('1', '1', '자기소개 제목1', '안녕하세요 :）', 'https://github.com/');
+INSERT INTO `linkpro`.`pro_pr_post` (`pr_post_id`, `pr_pro_id`, `pr_title`, `pr_contents`, `pr_github`) VALUES ('2', '2', '자기소개 제목2', '개발자 홍효식은 2015년 6월에 (주)오투오소프트를 설립하여 대표자를 역임 하였고 경영 및 소프트웨어 엔지니어로 활동 하고 있습니다. ', 'https://github.com/');
+INSERT INTO `linkpro`.`pro_pr_post` (`pr_post_id`, `pr_pro_id`, `pr_title`, `pr_contents`, `pr_github`) VALUES ('3', '3', '자기소개 제목3', '안녕하십니까?', 'https://github.com/');
+INSERT INTO `linkpro`.`pro_pr_post` (`pr_post_id`, `pr_pro_id`, `pr_title`, `pr_contents`, `pr_github`) VALUES ('4', '4', '자기소개 제목4', '안녕하세요 ! 고객님의 사업을 함께 시작하는 바른기획입니다. ', 'https://github.com/');
+INSERT INTO `linkpro`.`pro_pr_post` (`pr_post_id`, `pr_pro_id`, `pr_title`, `pr_contents`, `pr_github`) VALUES ('5', '5', '자기소개 제목5', '10년이상의 개발경력과 팀리더 경력을 가지고 있는 모바일 및 웹 개발팀장입니다.', 'https://github.com/');
+INSERT INTO `linkpro`.`pro_pr_post` (`pr_post_id`, `pr_pro_id`, `pr_title`, `pr_contents`, `pr_github`) VALUES ('6', '6', '자기소개 제목6', '서비스 설명', 'https://github.com/');
+INSERT INTO `linkpro`.`pro_pr_post` (`pr_post_id`, `pr_pro_id`, `pr_title`, `pr_contents`, `pr_github`) VALUES ('7', '7', '자기소개 제목7', '- 웹/앱 UI, UX 기획', 'https://github.com/');
+INSERT INTO `linkpro`.`pro_pr_post` (`pr_post_id`, `pr_pro_id`, `pr_title`, `pr_contents`, `pr_github`) VALUES ('8', '8', '자기소개 제목8', '안녕하세요!!  저희는 기획자 디자이너 개발자로 이루어진 스타트업입니다!', 'https://github.com/');
+INSERT INTO `linkpro`.`pro_pr_post` (`pr_post_id`, `pr_pro_id`, `pr_title`, `pr_contents`, `pr_github`) VALUES ('9', '9', '자기소개 제목9', '스타트업 대상', 'https://github.com/');
+INSERT INTO `linkpro`.`pro_pr_post` (`pr_post_id`, `pr_pro_id`, `pr_title`, `pr_contents`, `pr_github`) VALUES ('10', '10', '자기소개 제목10', '\"우리가 개발하는 건 당신의 꿈\"', 'https://github.com/');
+
+
+-- chat 테이블 데이터 삽입
+INSERT INTO `linkpro`.`chat` (`chat_id`, `chat_client_id`, `chat_pro_id`, `chat_send_time`, `chat_contents`) VALUES ('1', '4', '13', '2024-06-14 14:15:31', '안녕하세요 테스트 내용1');
+INSERT INTO `linkpro`.`chat` (`chat_id`, `chat_client_id`, `chat_pro_id`, `chat_send_time`, `chat_contents`) VALUES ('2', '46', '44', '2024-02-14 14:15:30', '안녕하세요 테스트 내용2');
+INSERT INTO `linkpro`.`chat` (`chat_id`, `chat_client_id`, `chat_pro_id`, `chat_send_time`, `chat_contents`) VALUES ('3', '36', '9', '2024-03-20 10:05:15', '안녕하세요 테스트 내용3');
+INSERT INTO `linkpro`.`chat` (`chat_id`, `chat_client_id`, `chat_pro_id`, `chat_send_time`, `chat_contents`) VALUES ('4', '100', '12', '2024-04-25 18:25:50', '안녕하세요 테스트 내용4');
+INSERT INTO `linkpro`.`chat` (`chat_id`, `chat_client_id`, `chat_pro_id`, `chat_send_time`, `chat_contents`) VALUES ('5', '42', '22', '2024-05-10 09:45:20', '안녕하세요 테스트 내용5');
+INSERT INTO `linkpro`.`chat` (`chat_id`, `chat_client_id`, `chat_pro_id`, `chat_send_time`, `chat_contents`) VALUES ('6', '33', '55', '2024-06-17 22:30:40', '안녕하세요 테스트 내용6');
+INSERT INTO `linkpro`.`chat` (`chat_id`, `chat_client_id`, `chat_pro_id`, `chat_send_time`, `chat_contents`) VALUES ('7', '66', '77', '2024-07-04 12:00:00', '안녕하세요 테스트 내용7');
+INSERT INTO `linkpro`.`chat` (`chat_id`, `chat_client_id`, `chat_pro_id`, `chat_send_time`, `chat_contents`) VALUES ('8', '88', '99', '2024-08-22 16:50:10', '안녕하세요 테스트 내용8');
+INSERT INTO `linkpro`.`chat` (`chat_id`, `chat_client_id`, `chat_pro_id`, `chat_send_time`, `chat_contents`) VALUES ('9', '8', '18', '2024-09-15 07:20:35', '안녕하세요 테스트 내용9');
+INSERT INTO `linkpro`.`chat` (`chat_id`, `chat_client_id`, `chat_pro_id`, `chat_send_time`, `chat_contents`) VALUES ('10', '99', '100', '2024-10-31 23:55:05', '안녕하세요 테스트 내용10');
+INSERT INTO `linkpro`.`chat` (`chat_contents`) VALUES ('');
+INSERT INTO `linkpro`.`chat` (`chat_contents`) VALUES ('');
+INSERT INTO `linkpro`.`chat` (`chat_contents`) VALUES ('');
