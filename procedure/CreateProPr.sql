f9673951303cacadf45d414f0b9a2f6e0c3b4d19
jeong seul ki <wisdom0405@kakao.com>
2024-06-02 22:53:11 +0900

    CreateProPR, UpdateProPR
    CreateProPR 자기소개글 작성 프로시저
    UpdateProPR 자기소개글 수정 프로시저 파일 추가

--- procedure/CreateProPr.sql
+++ procedure/CreateProPr.sql
@@ -1,5 +1,9 @@
 -- 개발자 자기소개글 작성 CreateProPR 프로시저
 
+-- 개발자 한명 당 한개의 자기소개 글을 작성할 수 있다.(pro_id 중복 불가)
+-- 개발자id(pro_id), 자기소개 제목(pr_title), 자기소개 내용(pr_contents), 깃허브 링크(pr_github) 순으로 입력하여 자기소개글 작성
+-- 만약 이미 작성한 자기소개글이 있는 상태에서 한번 더 작성하면 데이터가 삽입되지 않는다.
+
 select * from pro_pr_post;
 
 DELIMITER //
@@ -17,6 +21,6 @@ END //
 
 DELIMITER ;
 
--- 테스트
+-- 자기소개글 작성 테스트
 -- CALL CreateProPR(1, '자기소개 작성 테스트', '자기소개 내용', 'https://github.com/developer');
 
