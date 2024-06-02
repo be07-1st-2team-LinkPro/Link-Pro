957b607e0694ad9b632a42fb81e03622ac333ffa
jeong seul ki <wisdom0405@kakao.com>
2024-06-02 16:05:21 +0900

    delete files (ddl, jungha_ddl, linkpro_minji, stack_insert, stack_table.sql
    delete files (ddl, jungha_ddl, linkpro_minji, stack_insert, stack_table.sql

--- stack_table.sql
+++
@@ -1,6 +0,0 @@
--- 스택 카테고리 테이블
-CREATE TABLE IF NOT EXISTS `stack_category` (
-    `category_id` INT(11) NOT NULL AUTO_INCREMENT,  -- 카테고리 ID (자동 증가, 기본 키)
-    `category_name` VARCHAR(100) NOT NULL,  -- 카테고리 이름 (NULL 불가)
-    PRIMARY KEY (`category_id`)  -- 기본 키 설정
-);
