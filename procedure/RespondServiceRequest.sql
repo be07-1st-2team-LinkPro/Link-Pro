c8a1f153d1d364892877162553dddfd40513a519
JasonHwang <junghahwang0601@gmail.com>
2024-06-03 13:59:36 +0900

    응답 처리하는 쿼리 수정
    주문 수락 부분에서 수락하는 쿼리가 있어 기능 중복으로 주문 거절하는 프로시저로 변경

--- procedure/RespondServiceRequest.sql
+++ procedure/RespondServiceRequest.sql
@@ -1,13 +1,17 @@
 DELIMITER //
 
--- RespondServiceRequest 프로시저는 서비스 요청에 대한 수락 또는 거절을 처리합니다.
+-- RespondServiceRequest 프로시저는 서비스 요청에 대한 거절을 처리합니다.
 CREATE PROCEDURE RespondServiceRequest(
     IN p_order_id BIGINT,                     -- 주문 ID (입력 매개변수)
-    IN p_response ENUM('accept', 'reject')    -- 응답 (입력 매개변수: 'accept' 또는 'reject')
+    IN 전화번호 varchar(20)    -- 전화번호를 통해 proID 조회
 )
 BEGIN
+    declare proID bigint;
+    select pro_id into proID from pro where pro_tel = 전화번호;
     -- order_service 테이블에서 주어진 주문 ID에 해당하는 레코드의 상태를 업데이트합니다.
     UPDATE order_service
-    SET order_state = p_response              -- order_state 필드를 입력된 응답으로 설정합니다.
-    WHERE order_id = p_order_id;              -- 조건: 주문 ID가 입력된 주문 ID와 일치하는 레코드
+    SET order_state = 'reject'              -- order_state 필드를 입력된 응답으로 설정합니다.ㅣ
+    WHERE order_id = p_order_id and order_pro_id = proID; -- 조건: 주문 ID가 입력된 주문 ID와 일치하고 proID도 일치해야 하는 레코드
 END//
+
+DELIMITER //
